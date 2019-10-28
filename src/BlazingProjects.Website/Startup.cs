80f3f7f4e421b426629480524f6e3f13e634d6e3
Neville <live.neville@live.com>
2019-10-27 21:17:42 -0700

    updated config for azure signalr

--- src/BlazingProjects.Website/Startup.cs
+++ src/BlazingProjects.Website/Startup.cs
@@ -28,6 +28,7 @@ namespace BlazingProjects.Website
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSignalR().AddAzureSignalR("signalr");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
